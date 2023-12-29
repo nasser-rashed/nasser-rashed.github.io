ec11e61dba50f11c672fabb2d6b4e6a86cce977c
nasser-rashed <88663227+nasser-rashed@users.noreply.github.com>
2023-12-29 17:21:17 +0000

    updated

--- stopwatch/script.js
+++ stopwatch/script.js
@@ -1,17 +1,24 @@
 let startTime;
 let elapsedTime = 0;
 let timerInterval;
+let isRunning = false;
 
 function startStopwatch() {
+    if (!isRunning) {
   startTime = Date.now() - elapsedTime;
   timerInterval = setInterval(function() {
     elapsedTime = Date.now() - startTime;
     displayTime(elapsedTime);
   }, 10);
+  isRunning = true;
+}
 }
 
 function stopStopwatch() {
+    if (isRunning) {
   clearInterval(timerInterval);
+  isRunning = false;
+  }
 }
 
 function resetStopwatch() {
