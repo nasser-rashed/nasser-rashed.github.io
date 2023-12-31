2eebe6aa4541034dba8dd4f24448eff4fc12f9a9
nasser-rashed <rashednm.1@gmail.com>
2023-12-31 21:40:22 +0300

    hj

--- stopwatch/calc/script.js
+++ stopwatch/calc/script.js
@@ -1,7 +1,7 @@
 console.log("JS started.");
 a = document.querySelectorAll("input");
 console.log(a);
-function add(sign)
+function do_action(sign)
 {
     return eval( document.querySelectorAll("input")[0].value + sign +  document.querySelectorAll("input")[1].value )   ;
 }
