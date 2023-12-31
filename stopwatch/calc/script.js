01d2e5381b8734c658b71dcf210a4ae8d6679447
nasser-rashed <88663227+nasser-rashed@users.noreply.github.com>
2023-12-31 09:29:25 +0000

    sas

--- stopwatch/calc/script.js
+++ stopwatch/calc/script.js
@@ -1,3 +1,3 @@
 function updateD() {
-console.log(document.getElementsByClassName("container")) ; //document.getElementById('stopwatch').innerText = "Starting time!";
+console.log(document.getElementById('stopwatch').innerText ) ;
 }
